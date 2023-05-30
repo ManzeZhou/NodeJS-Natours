c8274d2e971776145e0920ae708ca824bc4f7fa6
Zhou Manze <manzezz1996@gmail.com>
2023-05-30 17:21:17 -0400

    create updateMe function to update user data

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const {
-  getAllUsers,
-  createUser,
-  getUser,
-  updateUser,
-  deleteUser,
+    getAllUsers,
+    createUser,
+    getUser,
+    updateUser,
+    deleteUser,
+    updateMe
 } = require('../controllers/userController');
 
 const authController = require('./../controllers/authController');
@@ -18,6 +19,7 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
+router.patch('/updateMe', authController.protect, updateMe)
 
 router.route('/').get(getAllUsers).post(createUser);
 
