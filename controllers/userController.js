b17e6a411f5a8e2472496322845ac81b95b10e90
Zhou Manze <manzezz1996@gmail.com>
2023-07-10 16:31:24 -0400

    allowed admin and guide to upload tour imageCover and images

--- controllers/userController.js
+++ controllers/userController.js
@@ -43,18 +43,18 @@ const upload = multer({
 exports.uploadUserPhoto = upload.single('photo');
 
 // resizing user photo
-exports.resizeUserPhoto = (req, res, next) => {
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next();
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
   //get file from memory: need square photo so height and weight should be same
-  sharp(req.file.buffer)
+  await sharp(req.file.buffer)
       .resize(500, 500)
       .toFormat('jpeg')
       .jpeg({quality: 90})
       .toFile(`public/img/users/${req.file.filename}`);
 
   next();
-}
+});
 
 exports.getAllUsers = factory.getAll(User);
 // exports.getAllUsers = catchAsync(async (req, res, next) => {
